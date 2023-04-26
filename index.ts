2e27250b862e71d2926e4405da62e6aa96b3988c
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-27 08:56:33 +0900

    Add R of CRUD

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 const port: number = 3000
+const env: any = process.env
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+const pgp = require('pg-promise')()
+const cn = {
+  host: env.POSTGRES_HOST,
+  port: env.POSTGRES_PORT,
+  database: env.POSTGRES_DB,
+  user: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD
+}
+const db = pgp(cn)
+
+app.get('/cruds', (req: Request, res: Response) => {
+  db.any('SELECT * FROM cruds')
+    .then((data: any) => {
+      res.send(data)
+    })
 })
 
 app.listen(port)
